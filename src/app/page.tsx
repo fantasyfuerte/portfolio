21f57be73c38a602711a16ec68f45460347315d4
fuerte <leandrofuerte75@gmail.com>
2025-01-26 19:16:40 +0100

    Practicing fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main>
-
+    <main className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl">
+        Welcome to my portfolio!
+      </h1>
     </main>
   );
 }
