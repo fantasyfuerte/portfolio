657fee7c7011f55de144631f315fa09cc778bdd9
fuerte <leandrofuerte75@gmail.com>
2025-01-27 13:46:50 +0100

    Styling navBAr item

--- src/components/navbar-item.tsx
+++ src/components/navbar-item.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface Props {
   href: string;
@@ -6,7 +8,16 @@ interface Props {
 }
 
 function NavBarItem({ href, icon }: Props) {
-  return <Link href={href}>{icon}</Link>;
+  const pathname = usePathname();
+
+  return (
+    <Link
+      href={href}
+      className={pathname !== href ? "text-secondary" : "text-primary"}
+    >
+      {icon}
+    </Link>
+  );
 }
 
 export default NavBarItem;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ import NavBarItem from "./navbar-item";
 function NavBar() {
   return (
     <nav className="flex justify-center w-full fixed bottom-5 ">
-      <div className="flex bg-middleColor rounded-2xl p-4 px-6 gap-8 text-primary">
+      <div className="flex bg-middleColor rounded-2xl p-4 px-6 gap-8">
         {navbarLinks.map((link) => (
           <NavBarItem key={link.name} href={link.href} icon={link.icon} />
         ))}
