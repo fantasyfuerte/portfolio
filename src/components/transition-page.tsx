6b8dedc03ffdf47251f26fdfd4e7732fabeb6e86
fuerte <leandrofuerte75@gmail.com>
2025-01-28 10:13:46 +0100

    Styling transitionpage

--- src/components/transition-page.tsx
+++ src/components/transition-page.tsx
@@ -8,7 +8,7 @@ function TransitionPage() {
     <AnimatePresence mode="wait">
       <div>
         <motion.div
-          className="fixed top-0 bottom-0 right-full w-screen z-50 bg-gradient-page"
+          className="fixed top-0 bottom-0 right-full w-screen z-50 bg-secondary "
           variants={transitionVariantsPage}
           initial="initial"
           animate="animate"
