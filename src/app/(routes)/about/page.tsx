bb83a6e739b0008783e7975e63a64da284904c8c
fuerte <leandrofuerte75@gmail.com>
2025-01-28 16:19:45 +0100

    Preparing about page and adding another future avatar

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -1,5 +1,11 @@
 import TransitionPage from "@/components/transition-page";
+import { Monsieur_La_Doulaise } from "next/font/google";
 import Image from "next/image";
+
+const brandFont = Monsieur_La_Doulaise({
+  weight: ["400"],
+  subsets: ["latin"],
+});
 
 function AboutPage() {
   return (
@@ -9,9 +15,15 @@ function AboutPage() {
         <Image
           alt="about-photo"
           src="/about-avatar.png"
-          width={250}
-          height={250}
+          width={200}
+          height={200}
         />
+        <section className="text-primary">
+          <h3 className="text-lg text-center py-2 mr-3">
+            <span className={`${brandFont.className} text-5xl`}>Fuerte</span>
+            &apos;s Skills
+          </h3>
+        </section>
       </main>
     </>
   );
