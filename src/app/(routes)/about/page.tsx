1981a8a22f166e10a8d02b48264d7f228a870eee
fuerte <leandrofuerte75@gmail.com>
2025-01-28 16:43:58 +0100

    Styling title

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -1,17 +1,11 @@
 import TransitionPage from "@/components/transition-page";
-import { Monsieur_La_Doulaise } from "next/font/google";
 import Image from "next/image";
-
-const brandFont = Monsieur_La_Doulaise({
-  weight: ["400"],
-  subsets: ["latin"],
-});
 
 function AboutPage() {
   return (
     <>
       <TransitionPage />
-      <main className="flex flex-col items-center">
+      <main className="flex flex-col items-center pt-5">
         <Image
           alt="about-photo"
           src="/about-avatar.png"
@@ -19,10 +13,8 @@ function AboutPage() {
           height={200}
         />
         <section className="text-primary">
-          <h3 className="text-lg text-center py-2 mr-3">
-            <span className={`${brandFont.className} text-5xl`}>Fuerte</span>
-            &apos;s Skills
-          </h3>
+          <h3 className="text-2xl text-center py-3 font-bold opacity-80">Tools & Skills</h3>
+        
         </section>
       </main>
     </>
