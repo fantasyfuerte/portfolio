a8c600310b6c7a8b57d35bab23dd68e3e8627861
fuerte <leandrofuerte75@gmail.com>
2025-01-28 09:53:13 +0100

    Refactoring and renaming variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Monsieur_La_Doulaise } from "next/font/google";
 import Image from "next/image";
 
-const playwritevn = Monsieur_La_Doulaise({
+const brandFont = Monsieur_La_Doulaise({
   weight: ["400"],
   subsets: ["latin"],
 });
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="text-primary">
         <h3 className={`text-lg text-center -ml-4`}>Welcome To</h3>
         <h3 className="text-lg text-center py-2">
-          <span className={`${playwritevn.className} text-6xl`}>Fuerte</span>
+          <span className={`${brandFont.className} text-6xl`}>Fuerte</span>
           &apos;s Showcase
         </h3>
       </div>
@@ -29,7 +29,7 @@ export default function Home() {
           unforgettable digital experiences.
         </p>
         <div className="flex justify-center gap-2 mt-8">
-          <button className=" bg-cta rounded-2xl px-4 py-2 text-basicBackground font-bold">
+          <button className="bg-cta rounded-2xl px-4 py-2 text-basicBackground font-bold">
             Contact Me
           </button>
           <button className="border-2 border-primary/80 rounded-2xl px-3 py-1 text-primary/80 font-semibold active:text-primary active:border-primary">
