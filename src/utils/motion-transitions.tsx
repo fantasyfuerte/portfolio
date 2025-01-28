051face6c0b69967b0aa66bbc880f01757c91e10
fuerte <leandrofuerte75@gmail.com>
2025-01-28 10:03:12 +0100

    Creating transition variants

---
+++ src/utils/motion-transitions.tsx
@@ -0,0 +1,14 @@
+export const transitionVariantsPage = {
+  initial: {
+    x: "100%",
+    height: "150%",
+  },
+  animate: {
+    x: "100%",
+    height: "0%",
+  },
+  exit: {
+    x: ["0%", "100%"],
+    width: ["0%", "100%"],
+  },
+};
