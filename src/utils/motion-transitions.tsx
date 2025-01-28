9a4f7cf2993f5b846f367e9bb91a79a2c0c7b034
fuerte <leandrofuerte75@gmail.com>
2025-01-28 10:13:58 +0100

    Changing transition variants

--- src/utils/motion-transitions.tsx
+++ src/utils/motion-transitions.tsx
@@ -1,11 +1,12 @@
 export const transitionVariantsPage = {
   initial: {
     x: "100%",
-    height: "150%",
+    width: "150%",
   },
   animate: {
     x: "100%",
-    height: "0%",
+    width: "0%",
+    borderRadius: "0 100% 40% 0",
   },
   exit: {
     x: ["0%", "100%"],
