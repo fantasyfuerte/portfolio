0c7517d4f9ec78d68e8002ab3141c4db8c9e3b50
fuerte <leandrofuerte75@gmail.com>
2025-01-28 17:07:54 +0100

    Maping frontend tools

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -1,20 +1,41 @@
 import TransitionPage from "@/components/transition-page";
 import Image from "next/image";
+import { tools } from "../../../../data";
 
 function AboutPage() {
   return (
     <>
       <TransitionPage />
-      <main className="flex flex-col items-center pt-5">
+      <main className="pt-5">
         <Image
+          className="mx-auto"
           alt="about-photo"
           src="/about-avatar.png"
           width={200}
           height={200}
         />
         <section className="text-primary">
-          <h3 className="text-2xl text-center py-3 font-bold opacity-80">Tools & Skills</h3>
-        
+          <h3 className="text-2xl text-center py-1 font-bold opacity-80">
+            Tools & Skills
+          </h3>
+          <article>
+            <h3 className="text-xl text-center py-1 font-bold opacity-80">
+              Frontend:
+            </h3>
+            <div className="grid grid-cols-4 gap-2 p-2 md:px-64 xl:px-96">
+              {tools.frontend.map((tool) => (
+                <div key={tool.name} className="border-2 border-red-950 h-20">
+                  <img
+                    src={tool.icon}
+                    alt={tool.name}
+                    width={50}
+                    height={50}
+                    className=""
+                  />
+                </div>
+              ))}
+            </div>
+          </article>
         </section>
       </main>
     </>
