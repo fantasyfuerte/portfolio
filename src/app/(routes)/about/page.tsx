6192dbd9aea6e2d0fbbc0990318d528fc3629f63
fuerte <leandrofuerte75@gmail.com>
2025-01-28 13:34:14 +0100

    Adding avatar to about page

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -1,10 +1,18 @@
 import TransitionPage from "@/components/transition-page";
+import Image from "next/image";
 
 function AboutPage() {
   return (
     <>
       <TransitionPage />
-      <div>From About page</div>
+      <main className="flex flex-col items-center">
+        <Image
+          alt="about-photo"
+          src="/about-avatar.png"
+          width={250}
+          height={250}
+        />
+      </main>
     </>
   );
 }
