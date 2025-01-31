1b925fc2826f576d1ec23203d06d23a0edc50482
fuerte <leandrofuerte75@gmail.com>
2025-01-31 07:49:16 +0100

    mapping projects

--- src/app/(routes)/projects/page.tsx
+++ src/app/(routes)/projects/page.tsx
@@ -1,10 +1,29 @@
 import TransitionPage from "@/components/transition-page";
+import { projects } from "../../../../data";
 
 function ProjectsPage() {
   return (
     <>
       <TransitionPage />
-      <div>From Projects page</div>
+      <main className="bg-basicBackground min-h-screen">
+        <div>
+          {projects.map((project) => (
+            <div
+              key={project.name}
+              className="flex flex-col gap-4 p-4 object-contain"
+            >
+              <img
+                src={project.image}
+                alt={project.name}
+                width={250}
+                height={250}
+                className=""
+              />
+              <h3>{project.name}</h3>
+            </div>
+          ))}
+        </div>
+      </main>
     </>
   );
 }
