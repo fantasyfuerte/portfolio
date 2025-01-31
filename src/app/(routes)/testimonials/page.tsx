f1369a0e2112958c8112e1b92dd14fb4aaf836d4
fuerte <leandrofuerte75@gmail.com>
2025-01-31 14:09:55 +0100

    Making testimonials page

---
+++ src/app/(routes)/testimonials/page.tsx
@@ -0,0 +1,11 @@
+function TestimonialsPage() {
+  return (
+    <div className="h-screen bg-basicBackground grid place-items-center">
+      <h5 className="text-center text-primary text-xl px-10">
+        Page testimonials is not available yet
+      </h5>
+    </div>
+  );
+}
+
+export default TestimonialsPage;
