7aa7e5224dfbcb94462eb56b5d694b074e1b40b8
fuerte <leandrofuerte75@gmail.com>
2025-01-31 10:35:01 +0100

    Styling project item

--- src/components/project-item.tsx
+++ src/components/project-item.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 function ProjectItem({ name, description, image, repo, demo }: Props) {
   return (
-    <article className="bg-secondary/80 rounded-xl m-4 p-4 text-primary">
+    <article className="bg-secondary/80 rounded-xl m-4 p-4 text-primary h-fit">
       <Image
         src={image}
         alt={name}
@@ -22,7 +22,7 @@ function ProjectItem({ name, description, image, repo, demo }: Props) {
       />
       <h3 className="text-xl font-bold">{name}</h3>
       <p className="text-sm opacity-80 font-medium">{description}</p>
-      <div className="pt-2 flex gap-2">
+      <div className="pt-2 flex gap-2 mt-auto">
         <Link
           href={repo}
           className="flex gap-2 items-center bg-basicBackground px-2 py-1 rounded-xl"
