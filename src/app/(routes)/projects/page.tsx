fc6794c5813e7fce9263b73b5b6f8ba78207095d
fuerte <leandrofuerte75@gmail.com>
2025-02-14 17:22:46 +0100

    Fixed title problem

--- src/app/(routes)/projects/page.tsx
+++ src/app/(routes)/projects/page.tsx
@@ -7,10 +7,10 @@ function ProjectsPage() {
     <>
       <TransitionPage />
       <main className="bg-basicBackground min-h-screen pb-24">
+        <h3 className="text-2xl font-bold text-primary text-center py-4">
+          Projects
+        </h3>
         <section className="flex flex-col gap-4 md:grid md:grid-cols-3">
-          <h3 className="text-2xl font-bold text-primary text-center py-4">
-            Projects
-          </h3>
           {projects.map((project) => (
             <ProjectItem
               name={project.name}
