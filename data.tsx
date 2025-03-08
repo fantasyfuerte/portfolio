ab8f444a50a38ac4cc2ae6702364a3d94a84dcc1
fuerte <leandrofuerte75@gmail.com>
2025-03-08 18:51:16 +0100

    contact ways order changed

--- data.tsx
+++ data.tsx
@@ -136,6 +136,11 @@ export const tools = {
 
 export const socialLinks = [
   {
+    name: "Gmail",
+    href: "mailto:leandrofuerte75@gmail.com?subject=Hello%20I%20came%20from%20your%20portfolio%20website",
+    icon: "/social-icons/gmail.svg",
+  },
+  {
     name: "Instagram",
     href: "https://www.instagram.com/fuerte.ts",
     icon: "/social-icons/instagram.svg",
@@ -145,9 +150,5 @@ export const socialLinks = [
     href: "https://www.github.com/fantasyfuerte",
     icon: "/social-icons/github.svg",
   },
-  {
-    name: "Gmail",
-    href: "mailto:leandrofuerte75@gmail.com?subject=Hello%20I%20came%20from%20your%20portfolio%20website",
-    icon: "/social-icons/gmail.svg",
-  },
+  
 ];
