5026790d234815114c254fb8e46eab04485659bc
fuerte <leandrofuerte75@gmail.com>
2025-03-23 08:51:16 +0100

    Improving semanthincs and refactoring

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -8,7 +8,7 @@ function AboutPage() {
     <>
       <TransitionPage />
       <main className="pt-5 pb-24 text-primary bg-basicBackground">
-        <div className="flex flex-col items-center gap-8">
+        <section className="flex flex-col items-center gap-8">
           <div className="w-48 h-48 md:w-64 md:h-64 relative overflow-hidden rounded-full border-4 border-middleColor shadow-md shadow-secondary">
             <Image
               src="/about.png"
@@ -18,28 +18,23 @@ function AboutPage() {
               className="object-cover scale-125 -ml-[9px]"
             />
           </div>
-
           <div className="flex-1 text-center">
             <h2 className="text-2xl md:text-3xl font-semibold mb-2 text-primary">
               Hello, I&apos;m Fuerte
             </h2>
-
             <div className="h-1 w-20 md:w-32 mx-auto mb-6 bg-cta"></div>
-
             <div className="mb-3 text-xl md:text-2xl font-semibold italic text-primary/80">
               &quot;Turning ideas into code, and code into experiences.&quot;
             </div>
-
             <p className="mb-6 text-lg md:mx-44 lg:mx-72 text-secondary font-medium">
               Every project is a step toward creating something impactful
               because great design isn&apos;t just seen, it&apos;s felt.
             </p>
-
             <button className="px-6 py-3 rounded-md text-white font-medium transition-transform hover:scale-105 bg-cta">
               Get in touch
             </button>
           </div>
-        </div>
+        </section>
 
         <section>
           <h3 className="text-2xl text-center py-3 font-bold opacity-80">
