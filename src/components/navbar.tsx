43471378f79af4ab3b4a149bd79f1d86e0571828
fuerte <leandrofuerte75@gmail.com>
2025-03-23 11:05:35 +0100

    Styling navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import NavBarItem from "./navbar-item";
 
 function NavBar() {
   return (
-    <nav className="flex justify-center w-full fixed bottom-5 ">
+    <nav className="flex justify-center w-full fixed bottom-0 pb-5 bg-gradient-to-t from-basicBackground via-10% via-basicBackground to-transparent">
       <div className="flex bg-middleColor rounded-2xl p-4 px-6 gap-8 shadow-lg shadow-primary/60">
         {navbarLinks.map((link) => (
           <NavBarItem key={link.name} href={link.href} icon={link.icon} />
