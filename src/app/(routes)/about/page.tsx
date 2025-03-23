62ce8b1f19793f9f58850aa931087106c81f69ee
fuerte <leandrofuerte75@gmail.com>
2025-03-23 08:15:48 +0100

    Changing aboutme page order

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -9,6 +9,37 @@ function AboutPage() {
     <>
       <TransitionPage />
       <main className="pt-5 pb-24 text-primary bg-basicBackground">
+        <section className="text-primary mt-10">
+          <ContactMe />
+          <article>
+            <h3 className="text-2xl text-center py-2 font-bold opacity-80">
+              Who am I?
+            </h3>
+            <Image
+              className="mx-auto"
+              alt="about-photo"
+              src="/about-avatar.png"
+              width={200}
+              height={200}
+            />
+            <p className="text-center p-4 opacity-80 font-semibold">
+              My name is{" "}
+              <span className="font-bold text-cta">Leandro Fuerte</span>. I am a{" "}
+              <span className="font-bold text-cta">Fullstack Developer</span>{" "}
+              with a specialized focus on{" "}
+              <span className="font-bold text-cta">frontend development</span>.
+              <br></br> I have a strong passion for creating{" "}
+              <span className="font-bold text-cta">
+                intuitive and user-friendly interfaces{" "}
+              </span>
+              that enhance the{" "}
+              <span className="font-bold text-cta">user experience</span>. My
+              goal is to continuously learn and innovate in order to build{" "}
+              <span className="font-bold text-cta">efficient and scalable</span>{" "}
+              applications that make a difference.
+            </p>
+          </article>
+        </section>
         <section>
           <h3 className="text-2xl text-center py-3 font-bold opacity-80">
             Tools & Skills
@@ -52,37 +83,6 @@ function AboutPage() {
                 <ToolItem key={tool.name} tool={tool} />
               ))}
             </div>
-          </article>
-        </section>
-        <section className="text-primary mt-10">
-          <ContactMe />
-          <article>
-            <h3 className="text-2xl text-center py-2 font-bold opacity-80">
-              Who am I?
-            </h3>
-            <Image
-              className="mx-auto"
-              alt="about-photo"
-              src="/about-avatar.png"
-              width={200}
-              height={200}
-            />
-            <p className="text-center p-4 opacity-80 font-semibold">
-              My name is{" "}
-              <span className="font-bold text-cta">Leandro Fuerte</span>. I am a{" "}
-              <span className="font-bold text-cta">Fullstack Developer</span>{" "}
-              with a specialized focus on{" "}
-              <span className="font-bold text-cta">frontend development</span>.
-              <br></br> I have a strong passion for creating{" "}
-              <span className="font-bold text-cta">
-                intuitive and user-friendly interfaces{" "}
-              </span>
-              that enhance the{" "}
-              <span className="font-bold text-cta">user experience</span>. My
-              goal is to continuously learn and innovate in order to build{" "}
-              <span className="font-bold text-cta">efficient and scalable</span>{" "}
-              applications that make a difference.
-            </p>
           </article>
         </section>
       </main>
