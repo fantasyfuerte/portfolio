2bfaf7b175d7e2f12dd33ffe09e79b6b4bb5123a
fuerte <leandrofuerte75@gmail.com>
2025-03-23 11:30:34 +0100

    Styling sociallink component

--- src/components/social-links.tsx
+++ src/components/social-links.tsx
@@ -2,12 +2,12 @@ import { socialLinks as links } from "../../data";
 
 export default function SocialLinks() {
   return (
-    <article>
+    <article className="mb-5 mt-3">
       <ul className="flex gap-2 justify-center">
         {links.map((link, index) => (
           <li key={index} className="hover:opacity-65 transition-opacity">
             <a href={link.href} target="_blank">
-              <img alt={link.name} width={20} height={20} src={link.icon} />
+              <img alt={link.name} width={22} height={20} src={link.icon} />
             </a>
           </li>
         ))}
